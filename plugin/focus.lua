8189e84bb87731c4ef718354b453cb0eaa663a42
Dylan Cairns-Howarth <dairnarth@dylancairns.co.uk>
2023-03-02 20:55:18 +0000

    Add focus autocmds using Styler and gruvbox-dim

--- plugin/focus.lua
+++ plugin/focus.lua
@@ -8,3 +8,27 @@ vim.api.nvim_create_autocmd('InsertLeave', {
   command = 'set nocursorline',
   group = 'focus'
 })
+
+vim.api.nvim_create_autocmd({'WinEnter', 'FocusGained'}, {
+  callback = function()
+    if vim.bo.buftype == '' then
+        vim.wo.relativenumber = true
+    end
+    require('styler').clear(0)
+    require('dairnarth.colours').highlight()
+  end,
+  group = 'focus'
+})
+vim.api.nvim_create_autocmd({'WinLeave', 'FocusLost'}, {
+  callback = function()
+    if vim.bo.buftype == '' then
+        vim.wo.relativenumber = false
+    end
+    require('styler').set_theme(0, {
+      colorscheme = 'gruvbox-dim',
+      background = 'dark'
+    })
+    require('dairnarth.colours').highlight()
+  end,
+  group = 'focus'
+})

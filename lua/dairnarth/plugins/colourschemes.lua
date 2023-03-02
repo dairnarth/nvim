90bc611124723022d085bb6d5e868164c71efcbf
Dylan Cairns-Howarth <dairnarth@dylancairns.co.uk>
2023-03-02 13:10:44 +0000

    Move to gruvbox.nvim; add gruvbox-dim; add styler.nvim

--- lua/dairnarth/plugins/colourschemes.lua
+++ lua/dairnarth/plugins/colourschemes.lua
@@ -1,9 +1,12 @@
 return {
   {
-    'morhetz/gruvbox',
+    'ellisonleao/gruvbox.nvim',
     lazy = false,
     priority = 1000,
     config = function()
+      require('gruvbox').setup({
+        italic = false
+      })
       vim.o.termguicolors = true
       vim.o.background = 'dark'
       vim.cmd('colorscheme gruvbox')
@@ -12,5 +15,19 @@ return {
       vim.cmd.highlight({'Search',    'guifg=#fabd2f', 'guibg=#282828', 'gui=bold,italic,underline'})
       vim.cmd.highlight({'IncSearch', 'guifg=#fabd2f', 'guibg=#282828', 'gui=bold,italic,reverse'})
     end
+  },
+  {
+    'dairnarth/gruvbox-dim.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('gruvbox-dim').setup({
+        italic = false
+      })
+    end
+  },
+  {
+      'folke/styler.nvim',
+      config = function() require'styler'.setup({themes = {}}) end
   },
 }
